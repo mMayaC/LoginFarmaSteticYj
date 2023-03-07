f45daff3c52fe74358d548914debadd69ff2fe7b
USUARIO <USUARIO@LAPTOP-5MBFKPCM>
2023-03-06 21:22:07 -0500

    Agregar archivos de proyecto.

---
+++ LoginFarmaSteticYj/Connection.cs
@@ -0,0 +1,101 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Path = System.IO.Path;
+
+namespace LoginFarmaSteticYj
+{
+    #region Uso de datos de un usuario
+    public class Login
+    {
+        public Login() { }
+
+        [PrimaryKey, AutoIncrement]
+        [MaxLength(10)]
+        public int Id { set; get; }
+
+        [MaxLength(20)]
+        public string User { get; set; }
+
+        [MaxLength(20)]
+        public string Password { get; set; }
+    }
+    #endregion
+
+    #region Manejo de datos y conexion a BD
+    public class Auxiliar
+    {
+        static object locker = new object();
+        SQLiteConnection Connection;
+        public Auxiliar() //Esto es un constructor
+        {
+            Connection = ConectarBD();
+            Connection.CreateTable<Login>();
+        }
+
+        public SQLite.SQLiteConnection ConectarBD()
+        {
+            SQLiteConnection conexionBaseDatos;
+            string nombreArchivo = "asesoria.db3";
+            string ruta = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            string completaRuta = Path.Combine(ruta, nombreArchivo);
+            conexionBaseDatos = new SQLiteConnection(completaRuta);
+            return conexionBaseDatos;
+        }
+
+        //Selecionar 1 registro
+        public Login SelecionarUno(string NombreUsuario, string ClaveUsuario)
+        {
+            lock (locker)
+            {
+                return Connection.Table<Login>().FirstOrDefault(x => x.User == NombreUsuario && x.Password == ClaveUsuario);
+            }
+        }
+
+        //Selecionar Muchos
+        public IEnumerable<Login> SeleccionarTodo()
+        {
+            lock (locker)
+            {
+                return (from i in Connection.Table<Login>() select i).ToList();
+            }
+        }
+
+        //Guardar
+        //Actualizar o insertar
+        public int Guardar(Login registro)
+        {
+            lock (locker)
+            {
+                if (registro.Id == 0)
+                {
+                    return Connection.Insert(registro);
+                }
+                else
+                {
+                    return Connection.Update(registro);
+                }
+            }
+        }
+        //Eliminar
+        public int Eliminar(int ID)
+        {
+            lock (locker)
+            {
+                return Connection.Delete<Login>(ID);
+            }
+        }
+
+    }
+    #endregion
+}
---
+++ LoginFarmaSteticYj/MainActivity.cs
@@ -0,0 +1,60 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace LoginFarmaSteticYj
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        ImageView imageView1;
+        EditText txtUser;
+        EditText txtPassword;
+        Button btnGetInto;
+        Button btnRegister;
+        Button btnDYFYPassword;
+
+   
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            imageView1=FindViewById<ImageView>(Resource.Id.imageView1);
+            txtUser=FindViewById<EditText>(Resource.Id.txtUser);
+            btnGetInto=FindViewById<Button>(Resource.Id.btnGetInto);
+            btnRegister=FindViewById<Button>(Resource.Id.btnRegister);
+            btnDYFYPassword=FindViewById<Button>((Resource.Id.btnDYFYPassword));
+
+            btnGetInto.Click += BtnGetInto_Click;
+            btnRegister.Click += BtnRegister_Click;
+            btnDYFYPassword.Click += BtnDYFYPassword_Click;
+        }
+
+        private void BtnDYFYPassword_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void BtnRegister_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void BtnGetInto_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
